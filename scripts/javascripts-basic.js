c70bb0c6260bea75fce610c34ff457f5555f531b
dangvanminhnhat192013 <nhatdang192013@gmail.com>
2025-09-14 10:15:40 +0700

    fix

--- scripts/javascripts-basic.js
+++ scripts/javascripts-basic.js
@@ -1,3 +1,18 @@
-console.log("nhat");
-/* xin chào */
-console.log(xin-chào);
+
+const doraemon = {
+      ten: "Doraemon",
+      quoctich: "nhatban",
+      xuatxu: "theky22",
+
+}
+const Doraemon = [
+    doraemon,
+    nobita,
+    {
+        ten: "Doraemon",
+        quoctich: "nhatban",
+        xuatxu: "theky22",
+    }
+        
+]
+                   
