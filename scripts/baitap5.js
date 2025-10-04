ae8f82963c2a65f4d685e2c1d60433ea2b275016
dangvanminhnhat192013 <nhatdang192013@gmail.com>
2025-10-04 09:56:38 +0700

    fix

--- scripts/baitap5.js
+++ scripts/baitap5.js
@@ -1,24 +1,44 @@
+/*
+document là nguyên toàn bộ trang web của mình. Nó là một phần tử chứa toàn bộ trang web của mình
+	document.addEventListener: toàn bộ trang web của mình được thêm 1 lắng nghe sự kiện
+	Và cụ thể sự kiện ở đây là: DOMContentLoaded -> khi trang web được load lên.
+	Vậy nguyên đoạn code document.addEventListener("DOMContentLoaded", function(){}) nghĩa là khi trang web được load lên thì sẽ chạy những đoạn code trong function
+	let form = document.getElementById("infoForm");
+	Tạo một biến form. Lấy thẻ form có id là "infoForm" gán vào biến form
+	let resultDiv = document.getElementById("result");
+	Tạo một biến resultDiv. Lấy thẻ có id là "result" gán vào biến resultDiv
+	if (form && resultDiv) { -> nếu form và resultDiv có giá trị -> điều kiện này bảo vệ trang web khỏi bị lỗi
+		form.addEventListener("submit", functành vi mặc định của form là load lại ion (e) {}) -> form lắng nghe một sự kiện là submit (khi bấm nút Hiển thị)
+		e.preventDefault(); -> chặn trang.
+		e là viết tắt của event
+		let name = document.getElementById("yourName").value.trim(); -> tạo biến name, lấy giá trị trong ô input có id là yourName, dùng hàm trim() để loại bỏ khoảng trắng thừa ở đầu và cuối chuỗi
+		let age = document.getElementById("yourAge").value.trim();
+
+		resultDiv.textContent = `Xin chào ${name}! Bạn ${age} tuổi.`; -> thay thế nội dung của phần resultDiv bằng nội dung mới
+	}
+*/
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("form");
-    const resultDiv = document.getElementById("result");
+	const form = document.getElementById("infoform");
+	const resultDiv = document.getElementById("result")
+	const sobimat = Math.round(Math.random() * 99) + 1;
+	
+	if (form && resultDiv) {
+		form.addEventListener("submit", function (e) {
+			e.preventDefault();
+			const songuoidungdoan = document.getElementById("secret").value.trim();
 
-    // Tạo số bí mật ngẫu nhiên từ 1 đến 100
-    let soBiMat = Math.round(Math.random() * 99) + 1;
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        const soNguoiDungDoan =(document.getElementById("secret").value.trim());
-
-        if (soNguoiDungDoan) {
-            if (soNguoiDungDoan === soBiMat) {
-                resultDiv.textContent = " Chúc mừng bạn đã đoán đúng!";
-            } else if (soNguoiDungDoan > soBiMat) {
-                resultDiv.textContent = " Số bạn đoán lớn hơn số bí mật";
-            } else {
-                resultDiv.textContent = " Số bạn đoán nhỏ hơn số bí mật";
-            }
-        } else {
-            resultDiv.textContent = " Vui lòng nhập một số hợp lệ từ 1 đến 100";
+			if (songuoidungdoan) {
+				if (songuoidungdoan = sobimat) {
+					resultDiv.textContent = "chúc mừng bạn số bạn đoán đúng!";
+			} else if (songuoidungdoan > sonimat) {
+				resultDiv.textContent = "Số bạn đoán lớn hơn số bị mật!";
+		} else {
+			resultDiv.textContent = "Số bạn đoán nhỏ hơn số bí mật!";
+		} 
+	else {
+			resultDiv.textContent = "Vui lòng nhập số bạn đoán!";
+		}
+	}
+})
         }
-    });
-});
+        })
--- scripts/playground.js
+++ scripts/playground.js
@@ -1,15 +1,35 @@
+//
+/*
+document là nguyên toàn bộ trang web của mình. Nó là một phần tử chứa toàn bộ trang web của mình
+	document.addEventListener: toàn bộ trang web của mình được thêm 1 lắng nghe sự kiện
+	Và cụ thể sự kiện ở đây là: DOMContentLoaded -> khi trang web được load lên.
+	Vậy nguyên đoạn code document.addEventListener("DOMContentLoaded", function(){}) nghĩa là khi trang web được load lên thì sẽ chạy những đoạn code trong function
+	let form = document.getElementById("infoForm");
+	Tạo một biến form. Lấy thẻ form có id là "infoForm" gán vào biến form
+	let resultDiv = document.getElementById("result");
+	Tạo một biến resultDiv. Lấy thẻ có id là "result" gán vào biến resultDiv
+	if (form && resultDiv) { -> nếu form và resultDiv có giá trị -> điều kiện này bảo vệ trang web khỏi bị lỗi
+		form.addEventListener("submit", functành vi mặc định của form là load lại ion (e) {}) -> form lắng nghe một sự kiện là submit (khi bấm nút Hiển thị)
+		e.preventDefault(); -> chặn trang.
+		e là viết tắt của event
+		let name = document.getElementById("yourName").value.trim(); -> tạo biến name, lấy giá trị trong ô input có id là yourName, dùng hàm trim() để loại bỏ khoảng trắng thừa ở đầu và cuối chuỗi
+		let age = document.getElementById("yourAge").value.trim();
+
+		resultDiv.textContent = `Xin chào ${name}! Bạn ${age} tuổi.`; -> thay thế nội dung của phần resultDiv bằng nội dung mới
+	}
+*/
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("form");
-    const resultDiv = document.getElementById("result");
+    const form = document.getElementById("infoform");
+    const resultDiv = document.getElementById("result")
+    if (form && resultDiv) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            const soA = document.getElementById("hangA").value.trim();
+            const soB = document.getElementById("hangB").value.trim();
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+            const tong = Number(soA) + Number(soB);
 
-        const soThuNhat = document.getElementById("so1").value.trim();
-        const soThuHai = document.getElementById("so2").value.trim();
-
-        const tong = Number(soThuNhat) + Number(soThuHai);
-
-        resultDiv.textContent = `✅ Tổng hai số là: ${tong}`;
-    });
-});
+            resultDiv.textContent = `✅ Tổng hai số là: ${tong}! `;
+        })
+    }
+})
